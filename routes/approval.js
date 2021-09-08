f5717592c808d94f78881507aae7255fd356fb37
hassen-abduse <hassen.abduse@gmail.com>
2021-09-08 23:53:42 +0300

    fixed an error

--- routes/approval.js
+++ routes/approval.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const auth = require('auth')
+const auth = require('./auth')
 const Applications = require('../models/applications')
 const Organization = require('../models/organizations')
 
