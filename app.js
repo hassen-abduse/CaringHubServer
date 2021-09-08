0605de82d626f21f16c3c6726cfe4ef13185e181
hassen-abduse <hassen.abduse@gmail.com>
2021-09-08 23:31:11 +0300

    modified app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
-app.use(cors());
+//app.use(cors());
 
 app.use(logger('dev'))
 app.use(express.json())
