2f7ba6703fbcc0fc89e0d4f7a5aab8886cbfb655
hassen-abduse <hassen.abduse@gmail.com>
2021-09-08 23:27:00 +0300

    added approval.js

---
+++ routes/approval.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const auth = require('auth')
+const Applications = require('../models/applications')
+const Organization = require('../models/organizations')
+
+const approveOrg = express.Router()
+const approveApp = express.Router()
+
+approveOrg.use(express.json())
+approveApp.use(express.json())
+
+approveOrg.route('/')
+    .put((req, res, next) => {
+        Organization.findByIdAndUpdate(req.body.orgId, {
+            $set: {
+                isApproved: true
+            }
+        }, { new: true })
+            .then((org) => {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json(org)
+            }, (err) => next(err))
+            .catch((err) => next(err))
+    })
+
+approveApp.route('/')
+    .put((req, res, next) => {
+        Applications.findByIdAndUpdate(req.body.appId, {
+            $set: {
+                accepted: true
+            }
+        }, { new: true })
+            .then((app) => {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json(app)
+            }, (err) => next(err))
+            .catch((err) => next(err))
+    })
+
+module.exports = {
+    approveApp,
+    approveOrg
+}
