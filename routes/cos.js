6d2144cad6e809077e7273bfc21b3071adefc745
hassen-abduse <hassen.abduse@gmail.com>
2021-10-19 08:25:58 +0300

    fixed an error

--- routes/cos.js
+++ routes/cos.js
@@ -3,15 +3,7 @@ const multer = require('multer')
 const multerS3 = require('multer-s3')
 
 
-const config = {
-    endpoint: 's3.ap.cloud-object-storage.appdomain.cloud',
-    apiKeyId: "qqUZmxXwFLhhJ_OUS5d6sUDrgy6yWndyo4MECrcvKbIG",
-    serviceInstanceId: "crn:v1:bluemix:public:cloud-object-storage:global:a/27f357709a524adf89e61384dd782600:76a869b7-8f0b-4e4d-9be9-459c28240e40::",
-    credentials: new IBM.Credentials('be8e4045e45f4a34990bac6ff9340872', '8195c18bf4c532395292874adf8afd8af833486c37cc2638', sessionToken = null),
-    signatureVersion: 'v2',
 
-
-}
 var cos = new IBM.S3(config);
 
 var upload = multer({
